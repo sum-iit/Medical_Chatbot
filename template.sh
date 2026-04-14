b7a1f7b3e812fcd6f364cbe8895bce3ee28c56f7
sum-iit <sumit.sah1824@gmail.com>
2026-04-14 23:15:52 +0530

    Folder Structure Created!

---
+++ template.sh
@@ -0,0 +1,16 @@
+mkdir -p src
+mkdir -p research
+
+# Creating Files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch reseach/trials.ipnby
+touch requirements.txt
+
+
+echo "Directory and files created successfully!"
